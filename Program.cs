cc31f6ed06aed99b1c9a4546385f01cab2465beb
Fran12302 <156135106+Fran12302@users.noreply.github.com>
2024-01-25 14:20:58 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,90 +1,141 @@
-﻿namespace zadaca_4
+﻿using System.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Graditelj
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            CarPicker picker = new CarPicker("Toyota");
-            
+            Client client=new Client();
+            client.Make();
         }
     }
-
-    public class CarShop
+   public interface IPermission
     {
-
-   
-     
-        public void GetCar(CarFactory factory)
-        {
-            Car car = factory.CreateCar();
-            car.Drive();
-        }
-
+        public void Allow();
+    }
+    public class EditPermission : IPermission
+    {
+        public void Allow()
+        { Console.WriteLine("User Has Edit Permission"); }
+    }
+    public class DeletePermission : IPermission
+    {
+        public void Allow()
+        { Console.WriteLine("User Has Delete Permission"); }
+    }
+    public class CreatePermission : IPermission
+    {
+        public void Allow()
+        { Console.WriteLine("User Has Create Permission"); }
+    }
+    public class ViewPermission : IPermission
+    {
+        public void Allow()
+        { Console.WriteLine("User Has View Permission"); }
     }
 
-    public class CarPicker
+    public class Account
     {
+        public List<IPermission> permissions;
+        public Account() { permissions = new List<IPermission>(); }
+    }
+    public interface AccountConfigurator
+    {
+        public AccountConfigurator AddEditPermission(EditPermission permission);
+        public AccountConfigurator AddDeletePermission(DeletePermission permission);
+        public AccountConfigurator AddCreatePermission(CreatePermission permission);
+        public AccountConfigurator AddViewPermission(ViewPermission permission);
+        public void Reset();
+        public Account Build();
         
-         
-        CarShop shop=new CarShop();
-       
-        public CarPicker(string name)
+    }
 
+    public class Configurator : AccountConfigurator
+    {
+        Account account;
+        public Configurator()
         {
+            this.account = new Account();
+        }
+
+        public void Reset()
+        {
+            account=new Account();
+        }
+        public AccountConfigurator AddEditPermission(EditPermission permission)
+        {
+            account.permissions.Add(permission);
+            return this;
             
-            if (name =="Toyota") {
-                shop.GetCar(new ToyotaFactory());
-               
-            }
-            if (name == "Ford")
+        }
+        public AccountConfigurator AddDeletePermission(DeletePermission permission)
+        {
+            account.permissions.Add(permission);
+            return this;
+        }
+        public AccountConfigurator AddCreatePermission(CreatePermission permission)
+        {
+            account.permissions.Add(permission);
+            return this;
+        }
+
+        public AccountConfigurator AddViewPermission(ViewPermission permission)
+        {
+            account.permissions.Add(permission);
+            return this;
+        }
+
+        public Account Build()
+        {
+            Account account = this.account;
+            Reset();
+            return account;
+        }
+    }
+
+    public class PresetPermissions
+    {
+        AccountConfigurator configurator;
+        public PresetPermissions(AccountConfigurator configurator)
+        {
+            this.configurator = configurator;
+        }
+        public Account Admin()
+        {
+            return configurator.AddEditPermission(new EditPermission()).AddDeletePermission(new DeletePermission()).AddCreatePermission(new CreatePermission()).AddViewPermission(new ViewPermission()).Build();
+            //give all permission
+        }
+        public Account User()
+        {
+            return configurator.AddViewPermission(new ViewPermission()).Build();    
+            //give view permission
+        }
+        public Account manager()
+        {
+            return configurator.AddEditPermission(new EditPermission()).Build();
+            //give create edit view
+        }
+    }
+
+    public class Client
+    {
+        PresetPermissions preset;
+        AccountConfigurator cof;
+
+        public void Make()
+        {
+            cof=new Configurator();
+            preset=new PresetPermissions(cof);
+            
+            Account manager=preset.manager();
+
+            foreach(IPermission per in manager.permissions)
             {
-                shop.GetCar(new FordFactory());
+                per.Allow();
             }
-        }
-        
-
-    }
-
-    public abstract class Car
-    {
-        public abstract void Drive();
-    }
-
-    public class Ford : Car
-    {
-        public override void Drive() { Console.WriteLine("Vozim ford"); }
-    }
-
-    public class Toyota : Car
-    {
-        public override void Drive()
-        {
-            Console.WriteLine("vozim toyotu");
+           
         }
     }
-
-    public abstract class CarFactory
-    {
-        public abstract Car CreateCar();
-    }
-
-    public class ToyotaFactory : CarFactory {
-
-        public override Car CreateCar()
-        {
-            return new Toyota();
-        }
-
-    }
-
-    public class FordFactory : CarFactory
-    {
-        public override Car CreateCar()
-        {
-            return new Ford();
-        }
-    }
-        
-
 }
-
